1a7f39ed0e67d2b93d4b783312009db6e1806a5b
Felix0708 <chuls0708@gmail.com>
2021-09-07 00:18:12 +0900

    form and css fixed

--- articles/forms.py
+++ articles/forms.py
@@ -1,9 +1,39 @@
 from django import forms
+from django.forms.widgets import TextInput, Textarea
 from .models import Article
 
 class ArticleForm(forms.ModelForm):
+    title = forms.CharField(
+        label='제목',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'my-title',
+                'placeholder': '제목을 입력하세요.',
+                'maxlength': 20,
+                'style' : 'border: solid 2px #D2691E; border-radius: 8px;'
+            }
+        ),
+        error_messages={
+            'required': '(공백 X, 제목을 입력하세요.)',
+        },
+    )
+    content = forms.CharField(
+        label='내용',
+        widget=forms.Textarea(
+            attrs={
+                'class': 'my-content',
+                'placeholder': '내용을 입력하세요.',
+                'rows': 5,
+                'cols': 50,
+                'style': 'border: solid 2px #D2691E; border-radius: 8px;'
+            }
+        ),
+        error_messages={
+            'required': '(공백 X, 내용을 입력하세요.)'
+        },
+    )
 
     class Meta:
         model = Article
-        # fields = '__all__'
-        fields =  ('title', 'content', 'img_path')
+        fields =  ('title', 'content',)
+        
--- articles/views.py
+++ articles/views.py
@@ -1,45 +1,32 @@
-from django.contrib.auth.models import User
-from articles.models import Article
-from django.shortcuts import render, redirect
 from django import forms
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_http_methods, require_POST, require_safe
+from articles.models import Article
 from articles.forms import ArticleForm
 
 
 # Create your views here.
-def new(request):
-    form = ArticleForm()
+@require_http_methods(['GET', 'POST'])
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        img_path = request.POST.get('img_path')
+        if form.is_valid():
+            article = form.save()
+            article = Article(img_path=img_path)
+            article.user = request.user
+            article.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
     context = {
         'form': form,
     }
-    return render(request, 'articles/new.html', context)
+    return render(request, 'articles/create.html', context)
 
 
-def create(request):
-    form = ArticleForm(request.POST)
-    #유효성 검사
-    if form.is_valid():
-        article = form.save()
-        article.user = request.user
-        article.save()
-        return redirect('articles:detail', article.pk)
-    return redirect('articles:new')
-
-
-    # # if request.user.is_authenticated:
-    # if request.method == "POST":
-    #     title = request.POST.get('title')
-    #     content = request.POST.get('content')
-    #     user = request.user
-    #     img_path = request.POST.get('img_path')
-    #     article = Article(title=title, content=content, user=user, img_path=img_path)
-    #     article.save()
-    #     return redirect('articles:detail', article.pk)
-    # # else:
-    # #     return redirect('accounts:login')
-    # else:
-    #     return redirect('articles:home')
-
-
+@require_safe
 def home(request):
     articles = Article.objects.order_by('-pk')
     context = {
@@ -48,45 +35,41 @@ def home(request):
     return render(request, 'articles/home.html', context)
 
 
+@require_safe
 def detail(request, pk):
-    article = Article.objects.get(pk=pk)
+    article = get_object_or_404(Article, pk=pk)
     context = {
         'article': article
     }
     return render(request, 'articles/detail.html', context)
 
 
-def edit(request, pk):
-    article = Article.objects.get(pk=pk)
-    context = {
-        'article': article
-    }
-    return render(request, 'articles/edit.html', context)
-
-
+@require_http_methods(['GET', 'POST'])
 def update(request, pk):
-    article = Article.objects.get(pk=pk)
+    article = get_object_or_404(Article, pk=pk)
     if request.method == 'POST':
         if article.user == request.user:
-            article.title = request.POST.get('title')
-            article.content = request.POST.get('content')
-            article.user = request.user
+            form = ArticleForm(request.POST, instance=article)
             article.img_path = request.POST.get('img_path')
-            article.save()
-            return redirect('articles:detail', article.pk)
-        else:
-            return redirect('articles:detail', article.pk)
+            if form.is_valid():
+                form.save()
+                article.user = request.user
+                article.save()
+                return redirect('articles:detail', article.pk)
     else:
-        return redirect('articles:detail', article.pk)
+        form = ArticleForm(instance=article)
+    context = {
+        'article': article,
+        'form': form
+    }
+    return render(request, 'articles/update.html', context)
 
 
+@require_POST
 def delete(request, pk):
-    article = Article.objects.get(pk=pk)
-    if request.method == 'POST':
-        if article.user == request.user:
-            article.delete()
-            return redirect('articles:home')
-        else:
-            return redirect('articles:detail', article.pk)
+    article = get_object_or_404(Article, pk=pk)
+    if article.user == request.user:
+        article.delete()
+        return redirect('articles:home')
     else:
         return redirect('articles:detail', article.pk)
